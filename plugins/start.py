8e00ad4d669a28d51fe15b94d13cf26751ccb54b
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-19 10:15:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,3 +3,4 @@ Read This First https://telegra.ph/file/4fd7aad706c87f581b246.jpg
 step 2 https://telegra.ph/file/46b92ebc4048f9b8617fc.jpg
 
 Step 3 https://telegra.ph/file/8ef08199c462b4d7eb491.jpg 
+ Change (bot, update) From Your Command According To Async Def client
