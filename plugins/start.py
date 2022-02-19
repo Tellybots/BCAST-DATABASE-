80396b266fe74996395ffc46ce0f064e239afacd
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-19 10:13:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,3 +1,5 @@
 Read This First https://telegra.ph/file/4fd7aad706c87f581b246.jpg
 
 step 2 https://telegra.ph/file/46b92ebc4048f9b8617fc.jpg
+
+Step 3 https://telegra.ph/file/8ef08199c462b4d7eb491.jpg ,, Must Read Step 3 ,4
